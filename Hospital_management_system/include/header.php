3f8b7b2da70cb58c3fd8cd7ffe7987a4d265fdc4
zeuss254 <ckarimi676@gmail.com>
2024-06-10 16:49:43 +0300

    admin resources

---
+++ Hospital_management_system/include/header.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- bootstrap cdn link  -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+
+  <!-- jquery cdn link -->
+  <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
+
+  <!-- jquery Google CDN -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+
+  <!-- fontawesome cdn link  -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.4.2/css/fontawesome.min.css">
+
+  <!-- fontawesome cdn for all assests -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+  <title>HMS</title>
+
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-info bg-info" style="display:flex;justify-content:space-between ;">
+
+
+    <a href="http://localhost:8080/Hospital%20management%20system/index.php" style="text-decoration: none;">
+      <h5 class="text-white">Hospital Management System</h5>
+    </a>
+
+
+    <div class="mr-auto"></div>
+
+    <ul class="navbar-nav">
+      <?php
+      if (isset($_SESSION['admin'])) { #Check whether a variable is empty. Also check whether the variable is set/declared
+
+        $user = $_SESSION['admin'];
+
+        echo '
+            <li class="nav-item"><a href="" class="nav-link text-white">', $user, '</a></li>
+            <li class="nav-item"><a href="logout.php" class="nav-link text-white">logout</a></li>
+';
+      } else {
+        echo '
+
+            <li class="nav-item"><a href="adminlogin.php" class="nav-link text-white">Admin</a></li>
+            <li class="nav-item"><a href="" class="nav-link text-white">Doctor</a></li>
+            <li class="nav-item"><a href="" class="nav-link text-white">Patient</a></li>
+';
+      }
+
+      ?>
+    </ul>
+  </nav>
+
+
+
+
+</body>
+
+</html>
---
+++ Hospital_management_system/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HMS home page</title>
+</head>
+
+<body>
+    <?php
+    include("include/header.php");  //page will appear after header.php
+
+    ?>
+    <div style="margin-top: 50px;"></div>
+
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3 mx-1 shadow">
+                    <img src="http://localhost:8080/Hospital%20management%20system/img/more information.jpeg" alt="" style="width: 100%;height:190px">
+                    <h5 class="text-center">click on the button for more information</h5>
+                    <a href="#">
+                        <button class="btn btn-success" style="margin-left:30%">More!!!</button>
+                    </a>
+                </div>
+
+                <div class="col-md-4 mx-1 shadow">
+                    <img src="http://localhost:8080/Hospital%20management%20system/img/patient.jpeg" alt="" style="width: 100%;">
+                    <h5 class="text-center">create account so that we can take good care of you</h5>
+                    <a href="#">
+                        <button class="btn btn-success" style="margin-left:20%">Create account!!!</button>
+                    </a>
+                </div>
+
+                <div class=" col-md-4 mx-1 shadow">
+                    <img src="http://localhost:8080/Hospital%20management%20system/img/docctor.jpeg" alt="" style="width: 100%;">
+
+                    <h5 class="text-center">We are looking for doctors</h5>
+
+                    <a href="#">
+                        <button class="btn btn-success" style="margin-left:30%">Apply now!!</button>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
